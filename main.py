d88bbd08c8c3f725ef87f9c11c8f6a8d3e3c50a0
Mehonal <116497904+mehonal@users.noreply.github.com>
2023-03-26 02:29:59 +0300

    added few more core functions and integrated them with menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from getkey import getkey
+import random
 
 class Session():
     name: str
@@ -12,14 +13,22 @@ class Session():
 
 sessions = []
 
-session = Session("Session 1")
-print("Session \"Session 1\" has been initialized.")
-
-sessions.append(session)
+def session_exists(name: str):
+    "Removes the session provided by name"
+    if sessions and sessions is not None:
+        for session in sessions:
+            if session.name == name:
+                return True
+    return False
 
 def init_session(name: str | None = None):
+    if name == None:
+        name = random.randint(100_000, 999_999)
+        while session_exists(name):
+            name = random.randint(100_000, 999_999)
     session = Session(name)
     sessions.append(session)
+    print("Successfully initialized session:")
     return session
 
 def remove_session(name: str):
@@ -41,19 +50,40 @@ def use_session(name: str | None = None):
                 return session
     return None
 
+def list_sessions():
+    "Returns the session provided by name"
+    if sessions and sessions is not None:
+        for session in sessions:
+            print(session.name)
+    else:
+        print("You have no active sessions.")
+
 def menu():
     print(
         """
-        1) Initialize new sesison
-        2) Send Web Request
-        3) Quit
-        q) Quit program
-
-        Please press a valid key to proceed: """, end = ""
+1) Initialize new sesison
+2) Send Web Request
+l) List sessions
+q) Quit program
+Please press a valid key to proceed: """, end = ""
     )
 
     selection = getkey()
     print(selection)
+    if selection == "q":
+        print("Quitting Simply Sessionized.")
+        exit()
+    elif selection == "l":
+        list_sessions()
+    elif selection == "1":
+        session_name = input('Enter new session name: ')
+        init_session(session_name)
+    elif selection == "2":
+        pass
+        
 
 
 print("Simply Sessionized Started.")
+
+while True:
+    menu()
