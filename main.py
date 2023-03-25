b677a3df45c56c73116e4ec929998408f329e12c
Mehonal <116497904+mehonal@users.noreply.github.com>
2023-03-26 02:19:03 +0300

    added core functions and menu

--- main.py
+++ main.py
@@ -1,1 +1,59 @@
-print("Simply Sessionized.")
+import requests
+from getkey import getkey
+
+class Session():
+    name: str
+    session: requests.Session
+
+    def __init__(self,name):
+        self.name = name
+        self.session = requests.Session()
+
+
+sessions = []
+
+session = Session("Session 1")
+print("Session \"Session 1\" has been initialized.")
+
+sessions.append(session)
+
+def init_session(name: str | None = None):
+    session = Session(name)
+    sessions.append(session)
+    return session
+
+def remove_session(name: str):
+    "Removes the session provided by name"
+    if sessions and sessions is not None:
+        for session in sessions:
+            if session.name == name:
+                sessions.remove(session)
+                return True
+    return False
+
+def use_session(name: str | None = None):
+    "Returns the session provided by name"
+    if sessions and sessions is not None:
+        if name == None:
+            return sessions[-1]
+        for session in sessions:
+            if session.name == name:
+                return session
+    return None
+
+def menu():
+    print(
+        """
+        1) Initialize new sesison
+        2) Send Web Request
+        3) Quit
+        q) Quit program
+
+        Please press a valid key to proceed: """, end = ""
+    )
+
+    selection = getkey()
+    print(selection)
+
+
+print("Simply Sessionized Started.")
